12bce7da13793259d295a39f02396e9c5690a402
Hein Haraldsen <hein.haraldsen@bekk.no>
2024-11-28 10:42:45 +0100

    Fix/deploy dev (#1)
    * Added APIversion and kind
    
    * Added CODEOWNERS
    
    * Added test controller
    
    * Adding Sykmelding Gateway Controller
    
    * Changed path from health to isAlive
    
    * Removed generation of -plain.jar
    
    * Added shadowjar
    
    * Added Spring Boot Actuator
    
    * Changed isAlive to health
    
    * Fixed Controller

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,12 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.0"
 	id("io.spring.dependency-management") version "1.1.6"
+	id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 val logbacksyslog4jVersion = "1.0.0"
 val logstashLogbackEncoderVersion = "8.0"
+val kotlinxVersion = "0.11.0"
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
@@ -25,8 +27,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("com.papertrailapp:logback-syslog4j:$logbacksyslog4jVersion")
 	implementation("net.logstash.logback:logstash-logback-encoder:$logstashLogbackEncoderVersion")
+	implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxVersion")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -41,3 +45,24 @@ kotlin {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+tasks.named<Jar>("jar") {
+	enabled = false
+}
+
+tasks {
+
+	shadowJar {
+		archiveBaseName.set("app")
+		archiveClassifier.set("")
+		isZip64 = true
+		manifest {
+			attributes["Main-Class"] = "no.nav.sykmeldinger.ApplicationKt"
+		}
+	}
+
+	bootJar {
+		archiveFileName = "app.jar"
+	}
+}
+
